29304ba291b442ce73313abe82b7a2d4e0e871ec
MayuriGanore24 <155288497+MayuriGanore24@users.noreply.github.com>
2025-04-08 13:39:18 +0530

    Add files via upload

---
+++ Server.java
@@ -0,0 +1,18 @@
+import java.rmi.Naming;
+
+public class Server
+{
+	public static void main(String[] args)
+	{
+		try
+		{
+			Servant s= new Servant();
+			Naming.rebind("Server",s);
+			System.out.println("Server is ready!!");
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+	}
+}		
